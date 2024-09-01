d22f8438e29ab823003918936581f8f38461afa9
shahrezqureshi <srq.rox@gmail.com>
2024-09-01 19:35:10 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 // Setup
 
 tallyCount = 0
-let displayVar = document.getElementById("displayCounter") 
+let displayVar = document.getElementById("displayCounter")
 let saveVar = document.getElementById("displaySaveCount")
 
 // Reset Function
 function tallyReset(){
     tallyCount = 0
     displayVar.textContent = tallyCount
-    saveVar.textContent = ""
 }
 
 // Increment Function
 function tallyIncrement() {
     tallyCount += 1
     displayVar.textContent = tallyCount
-}
-
-// Save Function
-function tallySave(){
-    saveVar.textContent += tallyCount + " - "
 }

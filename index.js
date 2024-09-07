c7efbdc3c8f950c8f7b01f96cbb9dd9d419fe069
shahrezqureshi <srq.rox@gmail.com>
2024-09-07 14:48:05 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-// The web-page element with an ID of 'displayCounter' is
-// being loaded into a variable called 'displayVar' for easy access
-let displayVar = document.getElementById("displayCounter")
+// Load the Dipslay Container
+
+var displayButton = document.getElementById('display_container')
+
+// Load Save Modal Elements for Modal Container and Modal Dialog
+var saveModal = document.getElementById('save_modal')
+var saveModalDialog = document.getElementById('save_modal_dialog')
+var saveButton = document.getElementById('displaySave')
+
+// Load Reset Modal Elements for Modal Container, Dialog and Buttons
+var resetModal = document.getElementById('reset_modal')
+var resetModalDialog = document.getElementById('reset_modal_dialog')
+var resetButton = document.getElementById('displayReset')
 
 // JSON.parse() function is being used to retrieve the string
 // value of tallyCountSaved key into a variable called tallyCount
 tallyCount = JSON.parse(localStorage.getItem('tallyCountSaved'))
+
+// The web-page element with an ID of 'displayCounter' is
+// being loaded into a variable called 'displayVar' for easy access
+let displayVar = document.getElementById("displayCounter")
 
 // Increment function is adding 1 to the tallyCount variable 
 // (which has the tallyCountSaved value loaded from localStorage)
@@ -19,7 +33,7 @@ const tallyIncrement = () => {
 // tallyCount into a string since localStorage can only save string 
 // values
 const tallySave = () => {
-    localStorage.setItem('tallyCountSaved', JSON.stringify(tallyCount))
+    localStorage.setItem('tallyCountSaved', JSON.stringify(tallyCount));
 }
 
 // tallyReset function is setting the tallyCount to 0, and saving
@@ -39,3 +53,42 @@ if (tallyCount === null) {
 } else {
         displayVar.textContent = tallyCount
 }
+
+// Adding eventListeners to Elements on the Document
+
+
+
+// 
+
+displayButton.addEventListener ("click", () => {
+    tallyIncrement()
+})
+
+
+// Save Button would Show Save Modal while Saving the Counter
+saveButton.addEventListener ("click", () => {
+    tallySave()
+    saveModal.classList.add('visible')
+})
+
+saveModal.addEventListener ("click", () => {
+    saveModal.classList.remove('visible')
+})
+
+saveModalDialog.addEventListener ("click", () => {
+    saveModal.classList.remove('visible')
+})
+
+// Reset Button would Show the Reset Modal seeking Confirmation for Resetting
+resetButton.addEventListener ("click", () => {
+    resetModal.classList.add('visible')
+})
+
+resetModal.addEventListener ("click", () => {
+    resetModal.classList.remove('visible')
+})
+
+resetModalDialog.addEventListener ("click", () => {
+    tallyReset()
+    resetModal.classList.remove('visible')
+})

5d420ade6b6a2a2f90d921e02df42070612c6c4a
shahrezqureshi <srq.rox@gmail.com>
2024-09-08 13:33:45 +0500

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -4,6 +4,7 @@ const themeSwitch = document.getElementById('theme_switch')
 
 const enableDarkMode = () => {
     document.body.classList.add('dark_mode')
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#000000');
     localStorage.setItem('darkModeStatus', 'active')
 }
 
